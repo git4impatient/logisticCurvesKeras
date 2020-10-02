4ede05e7d563cd814f24db0c64ac66e71b206255
Marty <lurie@lurie.biz>
2020-10-02 15:15:24 -0400

    Update 050gendata.sh

--- 050gendata.sh
+++ 050gendata.sh
@@ -6,14 +6,17 @@
 #./gendata.py 1000000 > mydata.txt
 #ls -l
 
-!hadoop fs -rm -r  s3a://cdp-sandbox-default-se/datalake/marty
-!hadoop fs -mkdir   s3a://cdp-sandbox-default-se/datalake/marty
-!hadoop fs -mkdir s3a://cdp-sandbox-default-se/datalake/marty/clotcuves
-!python 200gencurves.py | gzip >clotcurves.gz 
+!hadoop fs -rm -r  s3a://cdp-sandbox-default-se/datalake/marty/clotcurves
+#!hadoop fs -mkdir   s3a://cdp-sandbox-default-se/datalake/marty/clotcurves
+!hadoop fs -mkdir s3a://cdp-sandbox-default-se/datalake/marty/clotcurves
+#!python 200gencurves.py | gzip >clotcurves.gz 
 #hadoop fs -put - s3a://cdp-sandbox-default-se/datalake/marty/clotcuves/clotcuves.gz
+# no local store go direct to S3
 
-!hadoop fs -put clotcurves.gz  s3a://cdp-sandbox-default-se/datalake/marty/clotcuves
+!python 200gencurves.py | gzip | hadoop fs -put -   s3a://cdp-sandbox-default-se/datalake/marty/clotcurves/clotcurves.gz
 
-!hadoop fs -ls s3a://cdp-sandbox-default-se/datalake/marty/clotcuves
+#!hadoop fs -put clotcurves.gz  s3a://cdp-sandbox-default-se/datalake/marty/clotcurves
+
+!hadoop fs -ls s3a://cdp-sandbox-default-se/datalake/marty/clotcurves
 
 
